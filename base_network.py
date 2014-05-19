e0726d7fd0dfe5ccb7db47f2b569ba30fb4d1960
Achilleas <achilleas.k@gmail.com>
2014-05-19 23:11:32 +0300

    Calculate NPSS - Plot info for neuron with highest NPSS

--- base_network.py
+++ base_network.py
@@ -5,20 +5,23 @@ from brian.monitor import StateMonitor, SpikeMonitor
 from brian.units import mvolt, msecond, uamp, uvolt, second, hertz
 from brian import raster_plot, defaultclock
 from matplotlib.pyplot import plot, show, subplot, figure, scatter, axis
-from numpy import argmax
-import spikerlib
+import matplotlib.mlab as mlab
+import numpy as np
+import spikerlib as sl
 
 print("Preparing simulation ...")
 defaultclock.dt = dt = 0.1*msecond
-duration = 2*second
+duration = 10*second
+w = 2*msecond  # coincidence window for npss
 Vrest = -60*mvolt
+Vth = -50*mvolt
 tau = 20*msecond
 lif_eq = Equations("""
 dV/dt = (Vrest-V)/tau : volt
 I : volt
 """)
 lif_eq.prepare()
-lif_group = NeuronGroup(1000, lif_eq, threshold="V>-50*mvolt", reset=Vrest,
+lif_group = NeuronGroup(1000, lif_eq, threshold="V>Vth", reset=Vrest,
                        refractory=2*msecond)
 
 inp_group = PoissonGroup(100, rates=30*hertz)
@@ -37,31 +40,64 @@ network = Network(lif_group, inp_group, inp_conn, lif_conn,
 print("Running for %f seconds ..." % (duration))
 network.run(duration)
 print("Simulation run finished.")
-print("Plotting spikes ...")
-figure("Spike trains")
-subplot(211)
-raster_plot(input_mon)
-subplot(212)
-raster_plot(spike_mon)
-
 print("Performing Gaussian convolution ...")
-t, conv_spikes = spikerlib.tools.spikeconvolve(spike_mon, 5*msecond)
-print("Calculating pairwise Kreuz metric ...")
-# pairwise Kreuz on 1k trains -- might have to wait a while
-kr_t, kr_d = spikerlib.metrics.kreuz.pairwise_mp(spike_mon.spiketimes.values(),
-        0*second, duration, duration/(2*msecond))
-print("Plotting calculated stuff ...")
-figure("Synchrony")
-subplot(211)
+t, conv_spikes = sl.tools.spikeconvolve(spike_mon, 5*msecond)
+figure("Spike trains")
+subplot(311)
+raster_plot(input_mon)
+subplot(312)
+raster_plot(spike_mon)
+subplot(313)
 plot(t, conv_spikes)
-axis(xmin=0, xmax=duration)
-scatter(t[argmax(conv_spikes)], max(conv_spikes), s=10)
-print("Synchrony peaked at t = %f s" % (t[argmax(conv_spikes)]))
+axis(xmin=0, xmax=float(duration))
+scatter(t[np.argmax(conv_spikes)], max(conv_spikes), s=10)
+show(block=False)
+print("Synchrony peaked at t = %f s" % (t[np.argmax(conv_spikes)]))
+
+# let's run NPSS on all neurons and see what we get
+print("Calculating NPSS ...")
+npss = []
+max_idx = 0
+trace_mon.insert_spikes(spike_mon, Vth+30*mvolt)  # not really necessary, but I like it
+for v, sp in zip(trace_mon.values, spike_mon.spiketimes.itervalues()):
+    if len(sp) < 2:
+        npss.append([0])
+        continue
+    npss.append(sl.tools.npss(v, sp, Vrest, Vth, tau, w))
+    if max(npss[-1]) > max(npss[max_idx]):
+        max_idx = len(npss)-1
+
+# get winner's inputs for comparison
+input_matrix = inp_conn.W.todense()
+source_inps = mlab.find(input_matrix[:,max_idx])
+lif_matrix = lif_conn.W.todense()
+source_nrns = mlab.find(lif_matrix[:,max_idx])
+figure("NPSS")
+subplot(211)
+# scatter plot of inputs
+inp_idx = 0
+for src in source_inps:
+    y = np.zeros(len(input_mon[src]))+inp_idx
+    scatter(input_mon[src], y, c="b")
+    inp_idx += 1
+for src in source_nrns:
+    y = np.zeros(len(spike_mon[src]))+inp_idx
+    scatter(spike_mon[src], y, c="r")
+    inp_idx += 1
+axis(xmin=0, xmax=float(duration))
 subplot(212)
-plot(kr_t, kr_d)
-axis(xmin=0, xmax=duration)
+plot(spike_mon[max_idx][1:], npss[max_idx])
+axis(xmin=0, xmax=float(duration))
 show()
 
-# after that, start doing some connectivity inference using the measure
-# correlations
+
+#print("Calculating pairwise Kreuz metric ...")
+# pairwise Kreuz on 1k trains -- might have to wait a while
+#kr_t, kr_d = spikerlib.metrics.kreuz.pairwise_mp(spike_mon.spiketimes.values(),
+#        0*second, duration, duration/(2*msecond))
+# give it some blanks for now, until we write and debug NPSS
+#subplot(212)
+#plot(kr_t, kr_d)
+#axis(xmin=0, xmax=float(duration))
+
 print("All done!")
