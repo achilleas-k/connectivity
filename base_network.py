5d036ed9643cb7ac47b938807b11e261a4664424
Achilleas <achilleas.k@gmail.com>
2014-05-19 21:37:03 +0300

    Plots Kreuz and gauss convolution of pike train
    Kreuz calculation can take a while given the number of spike trains

--- base_network.py
+++ base_network.py
@@ -4,14 +4,13 @@ from brian.directcontrol import PoissonGroup
 from brian.monitor import StateMonitor, SpikeMonitor
 from brian.units import mvolt, msecond, uamp, uvolt, second, hertz
 from brian import raster_plot, defaultclock
-from matplotlib.pyplot import plot, show, subplot, figure
-from matplotlib.mlab import normpdf
-from numpy import array, convolve, arange, zeros
+from matplotlib.pyplot import plot, show, subplot, figure, scatter, axis
+from numpy import argmax
 import spikerlib
 
 print("Preparing simulation ...")
 defaultclock.dt = dt = 0.1*msecond
-duration = 1*second
+duration = 2*second
 Vrest = -60*mvolt
 tau = 20*msecond
 lif_eq = Equations("""
@@ -38,19 +37,29 @@ network = Network(lif_group, inp_group, inp_conn, lif_conn,
 print("Running for %f seconds ..." % (duration))
 network.run(duration)
 print("Simulation run finished.")
-print("Plotting stuff ...")
+print("Plotting spikes ...")
 figure("Spike trains")
 subplot(211)
 raster_plot(input_mon)
 subplot(212)
 raster_plot(spike_mon)
 
-# gaussian convolution on sum of spikes across time
-# there's definitely some synchronisation going on
-# figure out why
-t, conv_spikes = spikerlib.tools.spikeconvolve(spike_mon, 10*msecond)
-figure("Convolved output spikes")
+print("Performing Gaussian convolution ...")
+t, conv_spikes = spikerlib.tools.spikeconvolve(spike_mon, 5*msecond)
+print("Calculating pairwise Kreuz metric ...")
+# pairwise Kreuz on 1k trains -- might have to wait a while
+kr_t, kr_d = spikerlib.metrics.kreuz.pairwise_mp(spike_mon.spiketimes.values(),
+        0*second, duration, duration/(2*msecond))
+print("Plotting calculated stuff ...")
+figure("Synchrony")
+subplot(211)
 plot(t, conv_spikes)
+axis(xmin=0, xmax=duration)
+scatter(t[argmax(conv_spikes)], max(conv_spikes), s=10)
+print("Synchrony peaked at t = %f s" % (t[argmax(conv_spikes)]))
+subplot(212)
+plot(kr_t, kr_d)
+axis(xmin=0, xmax=duration)
 show()
 
 # after that, start doing some connectivity inference using the measure
