540f043b3c0565586e6f77a1ad19a5fd13d04c0a
Achilleas <achilleas.k@gmail.com>
2014-06-17 22:15:30 +0300

    Collect all data from npz and calculate relative error

--- plot_multiple_npz.py
+++ plot_multiple_npz.py
@@ -20,8 +20,14 @@ npss = []
 kreuz = []
 sync = []
 sigma = []
+N_in = []
+r_in = []
+w_in = []
 for idx, npz in enumerate(npzfiles):
     npzdata = np.load(npz)
+    N_in.extend(npzdata["N_in"])
+    r_in.extend(npzdata["r_in"])
+    w_in.extend(npzdata["w_in"])
     npss.extend(npzdata["npss"])
     kreuz.extend(npzdata["kreuz"])
     sync.extend(npzdata["S_in"])
@@ -35,6 +41,12 @@ npss = np.array(npss)
 kreuz = np.array(kreuz)
 npss_kr = np.sqrt(1-kreuz/0.3)-0.0148
 errors = npss-npss_kr
+relerr = np.zeros(len(npss))
+for idx in range(len(npss)):
+    if npss[idx]:
+        relerr[idx] = abs(npss[idx]-npss_kr[idx])/npss[idx]
+    else:
+        relerr[idx] = npss_kr[idx]
 plt.scatter(npss, npss_kr)
 plt.plot([0, 1], [0, 1], "k--")
 #for n, e in zip(npss, errors):
