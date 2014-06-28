39f6f301a316e46c9ad613f13ee5787d78248c73
Achilleas <achilleas.k@gmail.com>
2014-06-28 18:29:25 +0300

    Convolution and such

--- sinsignal.py
+++ sinsignal.py
@@ -1,42 +1,50 @@
 from brian import *
+import spikerlib as sl
 
 
 print("Preparing simulation ...")
 defaultclock.dt = dt = 0.1*ms
-duration = 20*second
+duration = 2*second
 w = 2*ms
 Vrest = -60*mV
 Vth = -50*mV
 tau = 20*ms
-
-DO THE CONVOLUTION THING
-
 n_ext = 100
 p_ext = 0.5
 p_int = 0.05
 w_ext = 0.5*mV
 r_ext = 10*Hz
 w_int = 0.1*mV
-lif_eq = Equations("""
-                    dV/dt = (Vrest-V)/tau+xi*I/sqrt(dt) : volt
-                    I = 10*mV*sin(t*5*Hz*pi) : volt
+lifeq = Equations("""
+                    dV/dt = (Vrest-V+I)/tau+xi*0.1*mvolt/sqrt(dt) : volt
+                    I = 5*mV*sin(t*5*Hz*pi) : volt
                    """)
-lif_eq.prepare()
-lifgroup = NeuronGroup(1000, lif_eq, threshold="V>Vth", reset=Vrest,
+lifeq.prepare()
+lifgroup = NeuronGroup(1000, lifeq, threshold="V>Vth", reset=Vrest,
                        refractory=2*ms)
 lifgroup.V = Vrest
 
-inp_group = PoissonGroup(n_ext, rates=r_ext)
-inp_conn = Connection(inp_group, lifgroup, weight=w_ext,
-                      sparseness=p_ext, fixed=True, delay=5*ms)
+inpgroup = PoissonGroup(n_ext, rates=r_ext)
+#inpconn = Connection(inpgroup, lifgroup, weight=w_ext,
+#                      sparseness=p_ext, fixed=True, delay=5*ms)
 
 print("Setting up monitors ...")
-# monitors
-inpmon = SpikeMonitor(inp_group)
+inpmon = SpikeMonitor(inpgroup)
 vmon = StateMonitor(lifgroup, 'V', record=True)
 spikemon = SpikeMonitor(lifgroup)
 print("Running simulation for %s ..." % (duration))
 run(duration, report="stdout")
-print("done.\nPlotting ...")
-raster_plot(spikemon)
-show()
+if spikemon.nspikes:
+    vmon.insert_spikes(spikemon, Vth+50*mV)
+    t = arange(0*ms, duration, dt)
+    print("done.\nPlotting ...")
+    figure()
+    subplot2grid((4, 1), (0, 0), rowspan=1)
+    plot(t, sin(t*5*Hz*pi))
+    subplot2grid((4, 1), (1, 0), rowspan=2)
+    raster_plot(spikemon)
+    t, convspikes = sl.tools.spikeconvolve(spikemon, 5*ms)
+    subplot2grid((4, 1), (3, 0), rowspan=3)
+    plot(t, convspikes)
+    axis(xmin=0, xmax=float(duration))
+    show()
