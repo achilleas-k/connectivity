8a2c91cba61a9745f3f0e5bb97fc3a465efab945
Achilleas <achilleas.k@gmail.com>
2014-07-01 20:42:09 +0300

    Downstream group

--- sinsignal.py
+++ sinsignal.py
@@ -13,27 +13,39 @@ n_ext = 100
 p_ext = 0.5
 w_ext = 0.1*mV
 r_ext = 10*Hz
-lifeq = Equations("""
+sineq = Equations("""
                     dV/dt = (Vrest-V+I)/tau+xi*0.2*mvolt/sqrt(dt) : volt
-                    I = 10*mV*sin(t*5*Hz*pi) : volt
+                    I = 5*mV*sin(t*5*Hz*pi) : volt
                    """)
-lifeq.prepare()
-lifgroup = NeuronGroup(1000, lifeq, threshold="V>Vth", reset=Vrest,
+sineq.prepare()
+singroup = NeuronGroup(1000, sineq, threshold="V>Vth", reset=Vrest,
                        refractory=2*ms)
-lifgroup.V = Vrest
+singroup.V = Vrest
 
-inpgroup = PoissonGroup(n_ext, rates=r_ext)
-inpconn = Connection(inpgroup, lifgroup, weight=w_ext,
-                      sparseness=p_ext, fixed=True, delay=1*ms)
+downstreameq = Equations("dV/dt = (Vrest-V)/tau : volt")
+downstreameq.prepare()
+downstreamgroup = NeuronGroup(10, downstreameq, threshold="V>Vth", reset=Vrest,
+                        refractory=2*ms)
+downstreamgroup.V = Vrest
+
+sintodnconn = Connection(singroup, downstreamgroup, weight=w_ext*5,
+                            delay=1*ms)
+
+#inpgroup = PoissonGroup(n_ext, rates=r_ext)
+#inpconn = Connection(inpgroup, lifgroup, weight=w_ext,
+#                      sparseness=p_ext, fixed=True, delay=1*ms)
 
 print("Setting up monitors ...")
-inpmon = SpikeMonitor(inpgroup)
-vmon = StateMonitor(lifgroup, 'V', record=True)
-spikemon = SpikeMonitor(lifgroup)
+#inpmon = SpikeMonitor(inpgroup)
+sinvmon = StateMonitor(singroup, 'V', record=True)
+sinspikemon = SpikeMonitor(singroup)
+downvmon = StateMonitor(downstreamgroup, 'V', record=True)
+downspikemon = SpikeMonitor(downstreamgroup)
 print("Running simulation for %s ..." % (duration))
 run(duration, report="stdout")
-if spikemon.nspikes:
-    vmon.insert_spikes(spikemon, Vth+50*mV)
+if sinspikemon.nspikes:
+    sinvmon.insert_spikes(sinspikemon, Vth+50*mV)
+    downvmon.insert_spikes(downspikemon, Vth+50*mV)
     t = arange(0*ms, duration, dt)
     print("done.\nPlotting ...")
     figure()
@@ -41,12 +53,16 @@ if spikemon.nspikes:
     plot(t, sin(t*5*Hz*pi))
     axis(xmin=0, xmax=float(duration))
     subplot2grid((4, 1), (1, 0), rowspan=2)
-    raster_plot(spikemon)
+    raster_plot(sinspikemon)
     axis(xmin=0, xmax=duration/ms)
-    t, convspikes = sl.tools.spikeconvolve(spikemon, 5*ms)
+    t, convspikes = sl.tools.spikeconvolve(sinspikemon, 5*ms)
     subplot2grid((4, 1), (3, 0), rowspan=3)
     plot(t, convspikes)
     axis(xmin=0, xmax=float(duration))
+
+    figure()
+    raster_plot(downspikemon)
+
     show()
 else:
     print("No spikes were fired.")
