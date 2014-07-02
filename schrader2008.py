64d2c8744fcae36b93a0fb3aa39a2f82b5c25cc6
Achilleas <achilleas.k@gmail.com>
2014-07-02 08:28:07 +0300

    StateMonitors kill RAM

--- schrader2008.py
+++ schrader2008.py
@@ -1,7 +1,7 @@
 from brian import *
 import spikerlib as sl
 
-def connect_inhibitory(group):
+def connect_recurrent(excgroup, inhgroup):
     print("Constructing inhibitory recurrent connections ...")
     # TODO: make inh2inhconn
     print("Constructing inhibitory to excitatory connections ...")
@@ -18,42 +18,38 @@ Vrest = 0*mV
 Vth = 20*mV
 tau = 20*ms
 C = 250*pF
-DC = 350*pA
+I = 350*pA
 Nexc = 10000
 Ninh = 10000
 lifeq = Equations("""
-dV/dt = (Vrest-V)/tau+DC/C : volt
+dV/dt = (Vrest-V)/tau+I/C : volt
 """)
+# I/C = 1.4 volt/second
 lifeq.prepare()
-excgrouup = NeuronGroup(Nexc, lifeq, threshold="V>Vth", reset=Vrest,
+excgroup = NeuronGroup(Nexc, lifeq, threshold="V>Vth", reset=Vrest,
                         refractory=2*ms)
-excgrouup.V = Vrest
+excgroup.V = Vrest
 inhgroup = NeuronGroup(Ninh, lifeq, threshold="V>Vth", reset=Vrest,
                        refractory=2*ms)
 inhgroup.V = Vrest
 exc2inhconn, ing2excconn, inh2inhconn = connect_recurrent(excgroup, inhgroup)
 
 print("Setting up monitors ...")
-inpmon = SpikeMonitor(inpgroup)
-vmon = StateMonitor(excgrouup, 'V', record=True)
-spikemon = SpikeMonitor(excgrouup)
+excvmon = StateMonitor(excgroup, 'V', record=1)
+excspikemon = SpikeMonitor(excgroup)
+#inhvmon = StateMonitor(inhgroup, 'V', record=True)
+inhspikemon = SpikeMonitor(inhgroup)
+
 print("Running simulation for %s ..." % (duration))
 run(duration, report="stdout")
-if spikemon.nspikes:
-    vmon.insert_spikes(spikemon, Vth+50*mV)
+if excspikemon.nspikes:
     t = arange(0*ms, duration, dt)
     print("done.\nPlotting ...")
     figure()
-    subplot2grid((4, 1), (0, 0), rowspan=1)
-    plot(t, sin(t*5*Hz*pi))
-    axis(xmin=0, xmax=float(duration))
-    subplot2grid((4, 1), (1, 0), rowspan=2)
-    raster_plot(spikemon)
-    axis(xmin=0, xmax=duration/ms)
-    t, convspikes = sl.tools.spikeconvolve(spikemon, 5*ms)
-    subplot2grid((4, 1), (3, 0), rowspan=3)
-    plot(t, convspikes)
-    axis(xmin=0, xmax=float(duration))
+    subplot(2,1,1)
+    raster_plot(excspikemon)
+    subplot(2,1,2)
+    raster_plot(inhspikemon)
     show()
 else:
     print("No spikes were fired.")
