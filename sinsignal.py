87467df77025bf8b4de26449d6ff9dd115226845
Achilleas <achilleas.k@gmail.com>
2014-07-03 15:37:03 +0300

    Indents

--- sinsignal.py
+++ sinsignal.py
@@ -14,9 +14,9 @@ p_ext = 0.5
 w_ext = 0.1*mV
 r_ext = 10*Hz
 sineq = Equations("""
-                    dV/dt = (Vrest-V+I)/tau+xi*0.2*mvolt/sqrt(dt) : volt
-                    I = 5*mV*sin(t*5*Hz*pi) : volt
-                   """)
+dV/dt = (Vrest-V+I)/tau+xi*0.2*mvolt/sqrt(dt) : volt
+I = 5*mV*sin(t*5*Hz*pi) : volt
+""")
 sineq.prepare()
 singroup = NeuronGroup(1000, sineq, threshold="V>Vth", reset=Vrest,
                        refractory=2*ms)
@@ -25,15 +25,15 @@ singroup.V = Vrest
 downstreameq = Equations("dV/dt = (Vrest-V)/tau : volt")
 downstreameq.prepare()
 downstreamgroup = NeuronGroup(10, downstreameq, threshold="V>Vth", reset=Vrest,
-                        refractory=2*ms)
+                              refractory=2*ms)
 downstreamgroup.V = Vrest
 
 sintodnconn = Connection(singroup, downstreamgroup, weight=w_ext*5,
-                            delay=1*ms)
+                         delay=1*ms)
 
 #inpgroup = PoissonGroup(n_ext, rates=r_ext)
 #inpconn = Connection(inpgroup, lifgroup, weight=w_ext,
-#                      sparseness=p_ext, fixed=True, delay=1*ms)
+#                     sparseness=p_ext, fixed=True, delay=1*ms)
 
 print("Setting up monitors ...")
 #inpmon = SpikeMonitor(inpgroup)
@@ -59,10 +59,8 @@ if sinspikemon.nspikes:
     subplot2grid((4, 1), (3, 0), rowspan=3)
     plot(t, convspikes)
     axis(xmin=0, xmax=float(duration))
-
     figure()
     raster_plot(downspikemon)
-
     show()
 else:
     print("No spikes were fired.")
