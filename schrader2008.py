52d8d2fac909cd9da189f6356592ce281f923668
Achilleas <achilleas.k@gmail.com>
2014-07-04 21:40:32 +0300

    Most likely working as intended - No alpha synapses

--- schrader2008.py
+++ schrader2008.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from brian import *
 import spikerlib as sl
 
@@ -19,18 +20,21 @@ def connect_recurrent(excgroup, inhgroup):
 def create_chains(excgroup):
     print("Creating synfire chains ...")
     nchains = 50
-    nchains = 10
+    nchains = 1
     nlinks = 20
     width = 100
     weight = 0.5*mV
     synfireconns = []
     for nc in range(nchains):
-        print("%i/%i ..." % (nc+1, nchains))
-        chainidxes = randint(0, len(excgroup), width*nlinks)
+        print("\r%i/%i ..." % (nc+1, nchains), end="")
+        sys.stdout.flush()
+        chainidxes = range(len(excgroup))
+        #shuffle(chainidxes)
+        chainidxes = array(chainidxes[:nlinks*width])
         chainidxes = chainidxes.reshape(nlinks, width)
         for prevlnk, nxtlnk in zip(chainidxes[:-1], chainidxes[1:]):
             delay = rand()*2.5*ms+0.5*ms  # uniform [0.5, 3]
-            layerconn = Connection(excgroup, excgroup, 'V', delay=delay)
+            layerconn = Connection(excgroup, excgroup, state='V', delay=delay)
             for pl in prevlnk:
                 for nl in nxtlnk:
                     layerconn[pl, nl] = weight
@@ -47,28 +51,39 @@ Vth = 20*mV
 tau = 20*ms
 C = 250*pF
 I = 350*pA
-Nexc = 1000
+Nexc = 4000
 Ninh = 1000
-lifeq = Equations("""
-dV/dt = (Vrest-V)/tau+I/C : volt
+tau_exc = 0.2*ms
+tau_inh = 0.6*ms
+a = 0*mV
+lifeq_exc = Equations("""
+dV/dt = (a-Vrest-V)/tau+I/C : volt
+#da/dt = (gIn-a)/tau_exc : volt
+#dgIn/dt = -gIn/tau_exc : volt
+""")
+lifeq_inh = Equations("""
+dV/dt = (a-Vrest-V)/tau+I/C : volt
+#da/dt = (gIn-a)/tau_inh : volt
+#dgIn/dt = -gIn/tau_inh : volt
 """)
 # I/C = 1.4 volt/second
-lifeq.prepare()
-excgroup = NeuronGroup(Nexc, lifeq, threshold="V>Vth", reset=Vrest,
-                        refractory=2*ms)
+lifeq_exc.prepare()
+lifeq_inh.prepare()
+excgroup = NeuronGroup(Nexc, lifeq_exc, threshold="V>Vth", reset=Vrest,
+                       refractory=2*ms)
 excgroup.V = Vrest
 network.add(excgroup)
-inhgroup = NeuronGroup(Ninh, lifeq, threshold="V>Vth", reset=Vrest,
+inhgroup = NeuronGroup(Ninh, lifeq_inh, threshold="V>Vth", reset=Vrest,
                        refractory=2*ms)
 inhgroup.V = Vrest
 network.add(inhgroup)
-exc2inhconn, inh2excconn, inh2inhconn = connect_recurrent(excgroup, inhgroup)
-network.add(exc2inhconn, inh2excconn, inh2inhconn)
+recurrent_conns = connect_recurrent(excgroup, inhgroup)
+network.add(*recurrent_conns)
 exc2excconn = create_chains(excgroup)
 network.add(*exc2excconn)
 
 print("Setting up monitors ...")
-excvmon = StateMonitor(excgroup, 'V', record=1)
+excvmon = StateMonitor(excgroup, 'V', record=range(100))
 excspikemon = SpikeMonitor(excgroup)
 #inhvmon = StateMonitor(inhgroup, 'V', record=True)
 inhspikemon = SpikeMonitor(inhgroup)
@@ -81,8 +96,10 @@ if excspikemon.nspikes:
     print("done.\nPlotting ...")
     figure()
     subplot(2,1,1)
+    title("Excitatory population")
     raster_plot(excspikemon)
     subplot(2,1,2)
+    title("Inhibitory population")
     raster_plot(inhspikemon)
     show()
 else:
