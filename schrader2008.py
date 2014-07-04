0d2bec40abdfbda9f385681615ea9d86094d5bcf
Achilleas <achilleas.k@gmail.com>
2014-07-04 23:39:02 +0300

    Alpha synapses

--- schrader2008.py
+++ schrader2008.py
@@ -2,25 +2,27 @@ from __future__ import print_function
 from brian import *
 import spikerlib as sl
 
+
 def connect_recurrent(excgroup, inhgroup):
     print("Constructing excitatory to inhibitory connections ...")
-    exc2inhconn = Connection(excgroup, inhgroup, state='V',
+    exc2inhconn = Connection(excgroup, inhgroup, state='gIn',
                              delay=(0.5*ms, 3*ms), weight=0.1*mV,
                              sparseness=0.1)
     print("Constructing inhibitory recurrent connections ...")
-    inh2inhconn = Connection(inhgroup, inhgroup, state='V',
+    inh2inhconn = Connection(inhgroup, inhgroup, state='gIn',
                              delay=(0.5*ms, 3*ms), weight=-0.6*mV,
                              sparseness=0.1)
     print("Constructing inhibitory to excitatory connections ...")
-    inh2excconn = Connection(inhgroup, excgroup, state='V',
+    inh2excconn = Connection(inhgroup, excgroup, state='gIn',
                              delay=(0.5*ms, 3*ms), weight=-0.6*mV,
                              sparseness=0.1)
     return exc2inhconn, inh2excconn, inh2inhconn
 
 def create_chains(excgroup):
+    import random as rnd
     print("Creating synfire chains ...")
     nchains = 50
-    nchains = 1
+    nchains = 5
     nlinks = 20
     width = 100
     weight = 0.5*mV
@@ -28,17 +30,16 @@ def create_chains(excgroup):
     for nc in range(nchains):
         print("\r%i/%i ..." % (nc+1, nchains), end="")
         sys.stdout.flush()
-        chainidxes = range(len(excgroup))
-        #shuffle(chainidxes)
-        chainidxes = array(chainidxes[:nlinks*width])
+        chainidxes = array(rnd.sample(range(len(excgroup)), nlinks*width))
         chainidxes = chainidxes.reshape(nlinks, width)
         for prevlnk, nxtlnk in zip(chainidxes[:-1], chainidxes[1:]):
             delay = rand()*2.5*ms+0.5*ms  # uniform [0.5, 3]
-            layerconn = Connection(excgroup, excgroup, state='V', delay=delay)
+            layerconn = Connection(excgroup, excgroup, state='gIn', delay=delay)
             for pl in prevlnk:
                 for nl in nxtlnk:
                     layerconn[pl, nl] = weight
             synfireconns.append(layerconn)
+    print()
     return synfireconns
 
 print("Preparing simulation ...")
@@ -55,16 +56,15 @@ Nexc = 4000
 Ninh = 1000
 tau_exc = 0.2*ms
 tau_inh = 0.6*ms
-a = 0*mV
 lifeq_exc = Equations("""
 dV/dt = (a-Vrest-V)/tau+I/C : volt
-#da/dt = (gIn-a)/tau_exc : volt
-#dgIn/dt = -gIn/tau_exc : volt
+da/dt = (gIn-a)/tau_exc : volt
+dgIn/dt = -gIn/tau_exc : volt
 """)
 lifeq_inh = Equations("""
 dV/dt = (a-Vrest-V)/tau+I/C : volt
-#da/dt = (gIn-a)/tau_inh : volt
-#dgIn/dt = -gIn/tau_inh : volt
+da/dt = (gIn-a)/tau_inh : volt
+dgIn/dt = -gIn/tau_inh : volt
 """)
 # I/C = 1.4 volt/second
 lifeq_exc.prepare()
@@ -83,7 +83,7 @@ exc2excconn = create_chains(excgroup)
 network.add(*exc2excconn)
 
 print("Setting up monitors ...")
-excvmon = StateMonitor(excgroup, 'V', record=range(100))
+excvmon = StateMonitor(excgroup, 'V', record=10)
 excspikemon = SpikeMonitor(excgroup)
 #inhvmon = StateMonitor(inhgroup, 'V', record=True)
 inhspikemon = SpikeMonitor(inhgroup)
@@ -92,15 +92,19 @@ network.add(excvmon, excspikemon, inhspikemon)
 print("Running simulation for %s ..." % (duration))
 network.run(duration, report="stdout")
 if excspikemon.nspikes:
+    avg_exc_rate = excspikemon.nspikes/duration/Nexc
+    avg_inh_rate = inhspikemon.nspikes/duration/Ninh
+    print("Average excitatory firing rate: %s" % (avg_exc_rate))
+    print("Average inhibitory firing rate: %s" % (avg_inh_rate))
     t = arange(0*ms, duration, dt)
     print("done.\nPlotting ...")
     figure()
     subplot(2,1,1)
-    title("Excitatory population")
     raster_plot(excspikemon)
+    title("Excitatory population")
     subplot(2,1,2)
-    title("Inhibitory population")
     raster_plot(inhspikemon)
+    title("Inhibitory population")
     show()
 else:
     print("No spikes were fired.")
